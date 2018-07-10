2e350b61ed6ebebeaffb87ea768e2b2e9f3cf9dd
lpy <forwardlpy@aliyun.com>
2018-07-10 23:20:18 +0800

    third commit

--- src/main/java/HelloGitHub.java
+++ src/main/java/HelloGitHub.java
@@ -4,5 +4,6 @@ public class HelloGitHub {
     public static void main(String[] args){
         System.out.println("hello github");
         System.out.println("hello world");
+        System.out.println("hello hadoop");
     }
 }
