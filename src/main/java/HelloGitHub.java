a62e75e6d8ed7939126de190781a95fd14d885e4
lpy <forwardlpy@aliyun.com>
2018-07-10 23:17:27 +0800

    second commit

--- src/main/java/HelloGitHub.java
+++ src/main/java/HelloGitHub.java
@@ -3,5 +3,6 @@ package main.java;
 public class HelloGitHub {
     public static void main(String[] args){
         System.out.println("hello github");
+        System.out.println("hello world");
     }
 }
